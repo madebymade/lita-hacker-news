91b0fb631773fcfb406ceb8177a3bae1ab570aec
Seb Ashton <seb@madebymade.co.uk>
2015-04-09 09:05:26 +0100

    Add RSpec config and apply CodeClimate coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,38 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
+require 'capybara/rspec'
+
 require "lita-hackers-news"
 require "lita/rspec"
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
 Lita.version_3_compatibility_mode = false
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+
+  config.filter_run :focus
+  config.run_all_when_everything_filtered = true
+
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+
+  config.profile_examples = 5
+
+  config.order = :random
+
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |expectations|
+    expectations.syntax = :expect
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = :expect
+
+    mocks.verify_partial_doubles = true
+  end
+end
